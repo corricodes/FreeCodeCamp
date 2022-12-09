3fc869897359eb62906a934318c19b6e4686b10b
Corri <95197153+corricodes@users.noreply.github.com>
2022-12-08 21:00:56 -0500

    added convert html entities

---
+++ JavaScriptAlgorithmsAndDataStructures/Intermediate Algorithm Scripting/convert-html-entities.js
@@ -0,0 +1,15 @@
+// Convert HTML Entities
+// Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+
+function convertHTML(str) {
+    const htmlEntities = {
+      "&": "&amp;",
+      "<": "&lt;",
+      ">": "&gt;",
+      '"': "&quot;",
+      "'": "&apos;",
+    }
+    return str.replace(/([&<>\"'])/g, match => htmlEntities[match]);
+  }
+  
+  convertHTML("Dolce & Gabbana");
